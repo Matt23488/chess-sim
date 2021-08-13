09df022b9c34a18dce7b3a90d84857fbe852bf1e
Matthew Bryant <matt23488@gmail.com>
2021-08-12 22:38:44 -0400

    UI stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
 import React, { useState } from 'react';
 import './App.css';
+import { Player } from './chess/game';
 import ChessBoard from './components/ChessBoard';
 import { useChessGame } from './utils/hooks';
 
 const App = () => {
     const chessGame = useChessGame();
-    const [perspectiveFlipEnabled, setPerspectiveFlipEnabled] = useState(true);
+    const [perspective, setPerspective] = useState<Player | 'turn'>('turn');
 
     return (
         <div className="App">
             <div className="App__Info">
                 <h3>Turn: {chessGame.playerTurn}</h3>
                 <label htmlFor="perspectiveEnabled">
-                    <input type="checkbox" id="perspectiveEnabled" checked={perspectiveFlipEnabled} onChange={() => setPerspectiveFlipEnabled(!perspectiveFlipEnabled)} />
-                    Enable Perspective Flip
+                    Player Perspective:&nbsp;
+                    <select id="perspectiveEnabled" value={perspective} onChange={e => setPerspective(e.target.value as Player | 'turn')}>
+                        <option value="turn">Turn</option>
+                        <option value="white">White</option>
+                        <option value="black">Black</option>
+                    </select>
                 </label>
+
                 <button onClick={chessGame.undo}>Undo</button>
             </div>
             <ChessBoard
                 onCellClick={chessGame.selectPosition}
                 chessGame={chessGame}
-                perspective={perspectiveFlipEnabled ? chessGame.playerTurn : 'white'}
+                perspective={perspective === 'turn' ? chessGame.playerTurn : perspective}
             />
         </div>
     );
